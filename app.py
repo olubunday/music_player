625884113a372d93e22f3331f7b1217f0d4b502f
Akinlowo Olumide Adeola <adeolaolumide9@gmail.com>
2023-12-21 11:01:15 +0100

    Updated app.py file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+from api import api_bp
 
 app = Flask(__name__)
 
@@ -6,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/static/music/<path:filename>')
+def download_file(filename):
+    return send_from_directory('static/music', filename, as_attachment=True)
 
+if __name__ == '__main__':
+    app.register_blueprint(api_bp)
+    app.run(debug=True)
